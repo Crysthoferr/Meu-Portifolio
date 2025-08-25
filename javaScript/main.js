8be76526b0902dd1bd224e3fb55cf9233834221f
Crysthofer <crysthoferluis040403@gmail.com>
2025-08-25 17:41:31 -0300

    atualização
    criei o botão de voltar ao topo e funcionalidade de menus navbar

---
+++ javaScript/main.js
@@ -0,0 +1,9 @@
+const botaoTopo = document.getElementById("botao-topo");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        botaoTopo.style.display = "block";
+    } else {
+        botaoTopo.style.display = "none";
+    }
+};
